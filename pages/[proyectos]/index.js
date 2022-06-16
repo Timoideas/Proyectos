2e09f9a9f93e6b492363097420da38c4119b76fa
Timoideas <Timoideas@hotmail.com>
2022-06-16 14:15:41 -0500

    proyectos Page Created

--- pages/[proyectos]/index.js
+++ pages/[proyectos]/index.js
@@ -1,14 +1,21 @@
 import style from 'styles/pages/[proyectos]/proyectos.module.css';
 import Head from 'heads/proyectos.head';
 import { Section, Body, Content } from 'components/timoideas/Timoideas.components';
+import { useRouter } from 'next/router';
+
 export default function Page() {
+	const router = useRouter();
+	const { proyectos } = router.query;
 	return (
 		<>
 			<Head />
 			<Body>
 				<Section>
 					<Content center>
-						<div className={style.Container}>Proyectos</div>
+						<div className={style.Container}>
+							<h1>Proyectos</h1>
+							<h3>{proyectos}</h3>
+						</div>
 					</Content>
 				</Section>
 			</Body>
